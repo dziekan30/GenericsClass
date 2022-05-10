aa88959c0e257f26b4e57a41a4efabfedf51ccdb
dziekan30 <dziekan30@gmail.com>
2022-05-10 17:33:32 -0500

    initial commit

---
+++ src/com/dziekan30/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package com.dziekan30;
+
+public class BaseballPlayer  extends Player{
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/com/dziekan30/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package com.dziekan30;
+
+public class SoccerPlayer extends Player{
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
